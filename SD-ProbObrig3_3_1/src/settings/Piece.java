02907666ecde366ee0dd1da39418ac55df594366
oliveirandre <oliveir.andre@ua.pt>
2019-05-16 12:33:11 +0100

    added project settings

---
+++ SD-ProbObrig3_3_1/src/settings/Piece.java
@@ -0,0 +1,41 @@
+package settings;
+
+import java.io.Serializable;
+import java.util.Random;
+
+/**
+ *
+ * @author Andre and Joao
+ */
+public class Piece implements Serializable {
+
+    EnumPiece p;
+
+	/**
+	 * Piece's constructor that initializes a new piece from a random EnumPiece.
+	 */
+	public Piece() {
+        p = EnumPiece.values()[new Random().nextInt(EnumPiece.values().length)];
+    }
+
+	/**
+	 * Retrieves a new typePiece
+	 * @return a EnumPiece
+	 */
+	public EnumPiece getTypePiece() {
+        return p;
+    }
+
+	/**
+	 * Retrieves the id of piece.
+	 * @return id of piece
+	 */
+	public int getIdTypePiece() {
+        return p.ordinal();
+    }
+
+    @Override
+    public String toString() {
+        return this.p.toString();
+    }
+}
