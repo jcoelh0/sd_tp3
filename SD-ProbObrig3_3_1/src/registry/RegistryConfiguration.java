76d87bdf3c12cd2995d97c3a12710946929b1cec
oliveirandre <oliveir.andre@ua.pt>
2019-05-16 12:19:41 +0100

    working on registry

---
+++ SD-ProbObrig3_3_1/src/registry/RegistryConfiguration.java
@@ -0,0 +1,13 @@
+package registry;
+
+/**
+ *
+ * @author André Oliveira
+ * @author João Coelho
+ */
+public class RegistryConfiguration {
+    
+    public static final String RMI_REGISTER_NAME = "RegisterHandler";
+    public static final String RMI_REGISTRY_HOSTNAME = "localhost";
+    
+}
