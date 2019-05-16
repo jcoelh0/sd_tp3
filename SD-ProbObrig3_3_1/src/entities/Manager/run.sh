ba1d38967ca6f3ae29b436a2412bb33c725e91ac
oliveirandre <oliveir.andre@ua.pt>
2019-05-16 12:40:59 +0100

    added entities

--- SD-ProbObrig3_3_1/src/entities/Manager/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -r classes;
-mkdir classes;
-javac -d "classes" -cp "../../../genclass.jar" ../../entities/Customer/*.java ../../entities/Customer/Interfaces/*.java  ../../entities/Customer/States/*.java ../../entities/Manager/*.java ../../entities/Manager/Interfaces/*.java ../../entities/Manager/States/*.java ../../entities/Mechanic/*.java ../../entities/Mechanic/Interfaces/*.java ../../entities/Mechanic/States/*.java ../../communication/*.java ../../messages/LoungeMessage/*.java ../../messages/OutsideWorldMessage/*.java ../../messages/ParkMessage/*.java ../../messages/RepairAreaMessage/*.java ../../messages/RepositoryMessage/*.java ../../messages/SupplierSiteMessage/*.java ../../settings/*.java ../../shared/Lounge/*.java ../../shared/OutsideWorld/*.java ../../shared/Park/*.java ../../shared/RepairArea/*.java ../../shared/Repository/*.java ../../shared/SupplierSite/*.java;
-java -classpath "classes" entities.Manager.ManagerRun
